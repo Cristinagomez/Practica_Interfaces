5b89131d9752300d7e2a28b3c1b8a76dab8a97d0
Cristinagomez <cgomezcampos@educa.madrid.org>
2023-02-23 17:28:28 +0100

    Cambios en añadir

--- anadir.js
+++ anadir.js
@@ -1,40 +1,35 @@
 import {Articulo, Ticket, TicketStore} from "./ticket.store.js"
 
+/*
+* Crear un ticket nuevo (no funciona)
+*/ 
 
-function añadir(){
-    var espacio = document.getElementById("añadido");
-    var campo = document.createElement("label");
-    campo.innerText = "Artículo";
-    var campoI = document.createElement("input");
-    campoI.type = "text";
-    campoI.id = "articulo";
-    campoI.name = "articulo";
-    campoI.placeholder = "Artículo";
-    var campo2 = document.createElement("label");
-    campo2.innerText = "Precio";
-    var campoII = document.createElement("input");
-    campoII.type = "text";
-    campoII.id = "precio";
-    campoII.name = "precio";
-    campoII.placeholder = "Precio";
-    var campo3 = document.createElement("label");
-    campo3.innerText = "Cantidad";
-    var campoIII = document.createElement("input");
-    campoIII.type = "text";
-    campoIII.id = "cantidad";
-    campoIII.name = "cantidad";
-    campoIII.placeholder = "Cantidad";
-    espacio.appendChild(campo);
-    espacio.appendChild(campoI);
-    espacio.appendChild(campo2);
-    espacio.appendChild(campoII);
-    espacio.appendChild(campo3);
-    espacio.appendChild(campoIII);
-  }
+function crear(){
+ let dia = document.getElementById("dia").value;
+ let mes = document.getElementById("mes").value;
+ let año = document.getElementById("año").value;
 
-let añadirB = document.getElementById("añadir");
+ let fecha = dia.toString() + "-" + mes.toString() + "-" + año.toString();
 
-añadirB.addEventListener("click", añadir);
+ let tienda = document.getElementById("tienda").value;
+ let articulo = document.getElementById("articulo").value;
+ let cantidad = document.getElementById("cantidad").value;
+ let precio = document.getElementById("precio").value;
+
+ console.log(fecha + " " + tienda + " " + articulo + " " + cantidad + " " + precio);
+
+ let ticket = ts.createTicket(tienda, fecha);
+ let art = ts.addArticuloToTicket(ticket,articulo,cantidad,precio);
+ console.log(ticket);
+ console.log(art);
+
+ ts.exportToLocalStorage();
+}
+
+let crearB = document.getElementById("crear");
+
+crearB.addEventListener("click", crear);
+
 
 
 let ts = new TicketStore();
@@ -45,6 +40,5 @@ console.log(`Hay ${totalTickets}`)
 
 let ticketList = ts.listAllTicket();
 
-console.log(`El primer ticket es id=${ticketList[0].id}`);
 
-ticketForm(ticketList[0].id, "id-pagina");
+
